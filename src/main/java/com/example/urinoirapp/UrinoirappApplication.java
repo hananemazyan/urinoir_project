94ab2bb74b12e33f3448aa77da12f0e350d28ec0
hanan <hananemzn16@gmail.com>
2024-05-02 08:49:48 +0100

    Setup the database structure all the files

---
+++ src/main/java/com/example/urinoirapp/UrinoirappApplication.java
@@ -0,0 +1,13 @@
+package com.example.urinoirapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrinoirappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrinoirappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/urinoirapp/UrinoirappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urinoirapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrinoirappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
